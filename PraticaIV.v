6cf8b4306bb4e932ee83dbb680e6aada5f4cb786
Gabriel Dutra <nesk.frz@gmail.com>
2018-06-14 14:48:19 -0300

    Máquina de estados cpu quase pronta

---
+++ PraticaIV.v
@@ -0,0 +1,10 @@
+module PraticaIV(SW, HEX0, HEX1, HEX2, HEX3, LEDR);
+	input [5:0] SW;
+	output [0:6] HEX0;
+	output [0:6] HEX1;
+	output [0:6] HEX2;
+	output [0:6] HEX3;
+	output [3:0] LEDR;
+	
+	
+endmodule
--- sm_cpu.v
+++ sm_cpu.v
@@ -1,13 +1,75 @@
 module sm_cpu(
+	clock,
+	write,
 	state,
 	writeMiss,
 	readMiss,
 	writeBack,
-	invalidate
+	invalidate,
+	currentState
 );
-
+	input clock;
+	input write;
 	input[1:0] state;
 	output reg writeMiss, readMiss, writeBack, invalidate;
 	
+	output reg[1:0] currentState;
 	
-end sm_cpu
+	initial begin
+		currentState = 2'b00;
+		writeMiss = 0;
+		readMiss = 0;
+		writeBack = 0;
+		invalidate = 0;
+	end
+	
+	always @(posedge clock)
+	begin
+		case(currentState)
+			2'b00: // Invalid
+			begin
+				case(state)
+					2'b00: // Invalid -> Invalid
+					begin
+					end
+					2'b01: // Invalid -> Shared
+					begin
+						// Place read miss on the bus
+					end
+					2'b10: // Invalid -> Modified
+					begin
+					end
+				endcase
+			end
+			2'b01: // Shared
+			begin
+				case(state)
+					2'b00: // Shared -> Invalid
+					begin
+					end
+					2'b01: // Shared -> Shared
+					begin
+					end
+					2'b10: // Shared -> Modified
+					begin
+					end
+				endcase
+			end
+			2'b10: // Modified
+			begin
+				case(state)
+					2'b00: // Modified -> Invalid
+					begin
+					end
+					2'b01: // Modified -> Shared
+					begin
+					end
+					2'b10: // Modified -> Modified
+					begin
+					end
+				endcase
+			end
+		endcase
+	end
+	
+endmodule
