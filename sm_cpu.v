05517b6e6794900211fc24f32c46d0583abc51f9
Gabriel Dutra <nesk.frz@gmail.com>
2018-06-15 08:45:33 -0300

    Máquina de estados CPU feita

--- sm_cpu.v
+++ sm_cpu.v
@@ -1,7 +1,7 @@
 module sm_cpu(
 	clock,
 	write,
-	state,
+	miss,
 	writeMiss,
 	readMiss,
 	writeBack,
@@ -10,7 +10,7 @@ module sm_cpu(
 );
 	input clock;
 	input write;
-	input[1:0] state;
+	input miss;
 	output reg writeMiss, readMiss, writeBack, invalidate;
 	
 	output reg[1:0] currentState;
@@ -28,46 +28,90 @@ module sm_cpu(
 		case(currentState)
 			2'b00: // Invalid
 			begin
-				case(state)
-					2'b00: // Invalid -> Invalid
-					begin
-					end
-					2'b01: // Invalid -> Shared
-					begin
-						// Place read miss on the bus
-					end
-					2'b10: // Invalid -> Modified
-					begin
-					end
-				endcase
+				if (write)
+				begin // CPU write
+					writeMiss = 1;
+					readMiss = 0;
+					writeBack = 0;
+					invalidate = 0;
+					currentState = 2'b10; // Modified
+				else // CPU read
+					writeMiss = 0;
+					readMiss = 1;
+					writeBack = 0;
+					invalidate = 0;
+					currentState = 2'b01; // Shared
+				end
 			end
 			2'b01: // Shared
 			begin
-				case(state)
-					2'b00: // Shared -> Invalid
-					begin
+				if (write)
+				begin // CPU write
+					if (miss)
+					begin // CPU write miss
+						writeMiss = 1;
+						readMiss = 0;
+						writeBack = 0;
+						invalidate = 0;
+						currentState = 2'b10; // Modified
+					else // CPU write
+						writeMiss = 0;
+						readMiss = 0;
+						writeBack = 0;
+						invalidate = 1;
+						currentState = 2'b10; // Modified
 					end
-					2'b01: // Shared -> Shared
-					begin
+				else // CPU read
+					if (miss)
+					begin // CPU read miss
+						writeMiss = 0;
+						readMiss = 1;
+						writeBack = 0;
+						invalidate = 0;
+						currentState = 2'b01; // Shared
+					else // CPU read hit
+						writeMiss = 0;
+						readMiss = 0;
+						writeBack = 0;
+						invalidate = 0;
+						currentState = 2'b01; // Shared
 					end
-					2'b10: // Shared -> Modified
-					begin
-					end
-				endcase
+				end
 			end
 			2'b10: // Modified
 			begin
-				case(state)
-					2'b00: // Modified -> Invalid
-					begin
+				if (write)
+				begin // CPU write
+					if (miss)
+					begin // CPU write miss
+						writeMiss = 1;
+						readMiss = 0;
+						writeBack = 1;
+						invalidate = 0;
+						currentState = 2'b10; // Modified
+					else // CPU write
+						writeMiss = 0;
+						readMiss = 0;
+						writeBack = 0;
+						invalidate = 0;
+						currentState = 2'b10; // Modified
 					end
-					2'b01: // Modified -> Shared
-					begin
+				else // CPU read
+					if (miss)
+					begin // CPU read miss
+						writeMiss = 0;
+						readMiss = 1;
+						writeBack = 1;
+						invalidate = 0;
+						currentState = 2'b01; // Shared
+					else // CPU read hit
+						writeMiss = 0;
+						readMiss = 0;
+						writeBack = 0;
+						invalidate = 0;
+						currentState = 2'b10; // Modified
 					end
-					2'b10: // Modified -> Modified
-					begin
-					end
-				endcase
+				end
 			end
 		endcase
 	end
