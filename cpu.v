c8df60dd804ab7ee31d019006646189bfc6f8211
Gabriel Dutra <nesk.frz@gmail.com>
2018-06-25 15:40:43 -0300

    a little bit more of cpu

--- cpu.v
+++ cpu.v
@@ -1,23 +1,22 @@
 module cpu(
 			clock,
 			instruction,
-			state,
 			address,
 			data_in,
-			current_state,
-			current_address,
-			current_data
+			readMiss,
+			writeMiss,
+			data_out
 );
 	input clock;
 	input instruction;
-	input [1:0] state;
 	input [2:0] address;
 	input [3:0] data_in;
-	output reg [1:0] current_state;
-	output reg [2:0] current_address;
-	output reg [3:0] current_data;
+	output readMiss;
+	output writeMiss;
+	output reg [3:0] data_out;
 	
 	reg current_cb;
+	reg [1:0] state;
 	
 	wire [1:0] current_state_cb1;
 	wire [2:0] current_address_cb1;
@@ -28,41 +27,54 @@ module cpu(
 	wire [3:0] current_data_cb2;
 	
 	wire write = instruction;
+	wire readMiss_sm1, readMiss_sm2;
+	
+	wire miss_sm1 = ~address[0] & state == 2'b00 & address != current_address_cb1;
+	wire miss_sm2 = address[0] & state == 2'b00 & address != current_address_cb2;
+	
+	wire write_sm1 = ~address[0] & ~state[1] & ~state[0] & write;
+	wire write_sm2 = address[0] & ~state[1] & ~state[0] & write;
+	
+	assign readMiss = readMiss_sm1 | readMiss_sm2;
+	assign writeMiss = writeMiss_sm1 | writeMiss_sm2;
 	
 	initial begin
-		current_state = 2'b00; // Invalid
-		current_address = address;
-		current_data = 4'b0000;
-		current_cb = 0;
+		data_out = 4'b0000;
+		state = 2'b00;
 	end
+	
+	sm_cpu sm1(clock,	write_sm1, miss_sm1,	writeMiss_sm1,	readMiss_sm1, writeBack_sm1, invalidate_sm1,	currentState_sm1);
+	sm_cpu sm2(clock,	write_sm2, miss_sm2,	writeMiss_sm2,	readMiss_sm2, writeBack_sm2, invalidate_sm2,	currentState_sm2);
 	
 	cache_block cb1(clock, write & ~current_cb, state & ~{2{state}}, address & ~{3{address}}, data_in & ~{4{data_in}}, current_state_cb1, current_address_cb1, current_data_cb1);
 	cache_block cb2(clock, write & current_cb, state & {2{state}}, adrress & {3{address}}, data_in & {4{data_in}}, current_state_cb2, current_address_cb2, current_data_cb2);
 	
 	always @(posedge clock)
 	begin
+	
+		case(state)
+			2'b00: // Verify state machine
+			begin
+				state = 2'b01;
+			end
+			2'b01:
+			begin
+				state = 2'b00;
+			end
+		endcase
 		
 		// Direct Mapping
 		if(address[0])
 		begin
 			current_cb = 0;
-			current_state = current_state_cb1;
-			current_address = current_address_cb1;
-			current_data = current_data_cb1;
 		end
 		else
 		begin
 			current_cb = 1;
-			current_state = current_state_cb2;
-			current_address = current_address_cb2;
-			current_data = current_data_cb2;
 		end
 		
 		if(write)
 		begin
-			current_state = state;
-			current_address = address;
-			current_data = data_in;
 		end
 	end
 
